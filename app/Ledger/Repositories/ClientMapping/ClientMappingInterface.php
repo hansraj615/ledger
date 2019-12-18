9c7283b4d1f00364ba279874a6478d7c12a70f20
Pramode Kumar Mahato <Pramode.Mahato@gds.ey.com>
2019-12-18 23:37:26 +0530

    removed line in ledgerinterface

--- app/Ledger/Repositories/ClientMapping/ClientMappingInterface.php
+++ app/Ledger/Repositories/ClientMapping/ClientMappingInterface.php
@@ -8,9 +8,6 @@ interface ClientMappingInterface
      public function getAllMapping();
      public function storeClientMapping($request);
      public function searchSubClient($keyword = null);
-     // public function storeSubCompanyStock($request);
-     // public function getAllOpening();
-     // public function deleteSubCompanyStock($id);
 
     
 }
